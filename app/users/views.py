aecec3235d9ca8129374dcb9c3766ffdec34ae23
Simeonone <simeon.kengere@gmail.com>
2024-05-28 10:46:08 +0300

    updated the run.py function

--- app/users/views.py
+++ app/users/views.py
@@ -1,5 +1,6 @@
-from . import users_bp
-from flask import jsonify
+from flask import Blueprint, jsonify
+
+users_bp = Blueprint('users', __name__)
 
 @users_bp.route('/users')
 def get_users():
--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-from app import create_app
+from flask import Flask
 from app.users.views import users_bp
 from app.products.views import products_bp
 
-app = create_app()
+app = Flask(__name__)
 
 # Register Blueprints
 app.register_blueprint(users_bp)
